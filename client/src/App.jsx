cf935d327d6a0b95770d214913f1e5f3f4f9e626
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-05-03 22:42:02 +0530

    client server initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl bg-red-500 text-yellow-300">hello</h1>
+    </div>
+  )
+}
+
+export default App
