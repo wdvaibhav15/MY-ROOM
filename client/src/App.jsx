459c725770ca8f8c5c7d8ed3ca44e6a53fe42f62
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-05-03 23:41:19 +0530

    navbar stablished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes('/owner');
   return (
-    <div>
-      <h1 className="text-3xl bg-red-500 text-yellow-300">hello</h1>
-    </div>
+    <>
+      {!isOwnerPath && <Navbar />}
+    </>
   )
 }
 
